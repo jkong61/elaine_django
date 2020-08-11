a964625ade1c0c870250bfeaaf1325d5038c101a
joako407 <joako407@gmail.com>
2020-08-11 20:05:28 +0800

    added forms for specific inspections

--- inspection/forms.py
+++ inspection/forms.py
@@ -1,3 +1,11 @@
 from django.forms import ModelForm
 from .models import VisualInspection, MPIInpection, PipeworkNDEInspection, AnnualInspection, MajorInspection, PrePostJobInspection, CalibrationInspection
 
+class GenericInspectionForm(ModelForm):
+    class Meta:
+        fields = ['validity_start_date','material_instance','validity','in_use']
+        read_only = ['validity_end_date']
+
+class CalibrationInspectionForm(GenericInspectionForm):
+    class Meta(GenericInspectionForm.Meta):
+        model = CalibrationInspection
--- inspection/views.py
+++ inspection/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import CalibrationInspection
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .forms import CalibrationInspectionForm
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
 
 # Create your views here.
-class CalibrationCreateView(CreateView):
+class CalibrationCreateView(FormView):
     model = CalibrationInspection
-    fields = ['validity_start_date','material_instance']
+    form_class = CalibrationInspectionForm
     template_name = 'inspection/add.html'
+
+    def get_context_data(self, **kwargs):
+    # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Calibration Inspection'
+        context['header'] = 'Calibration Certification'
+        return context
