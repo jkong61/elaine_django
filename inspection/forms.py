8ae94e1ae79a887d2f45160382870408f679711a
joako407 <joako407@gmail.com>
2020-08-13 10:29:41 +0800

    implement test for adding instance

--- inspection/forms.py
+++ inspection/forms.py
@@ -3,6 +3,7 @@ from core.models import Instance
 from .models import SkidVisualInspection, SkidMPIInpection, SlingMPIInpection, SlingVisualInspection, PipeworkNDEInspection, AnnualInspection, MajorInspection, PrePostJobInspection, CalibrationInspection
 
 class GenericInspectionForm(forms.ModelForm):
+
     class Meta:
         fields = ['validity_start_date','material_instance','validity','in_use']
         read_only = ['validity_end_date']
---
+++ inspection/tests/test_form.py
@@ -0,0 +1,4 @@
+from django.tests import TestCase
+
+class TestSaveInstanceForms(TestCase):
+    pass
